e13e9c5b6adaf2e2391e6f1924e271c89f1af6a5
Yusi0114 <132541872+Yusi0114@users.noreply.github.com>
2025-03-26 13:30:48 +0800

    修改BUG

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import firebase_admin
 import pyrebase
-from firebase_admin import credentials, firestore,storage
+from firebase_admin import credentials, firestore
 from flask import Flask, render_template, request, redirect, session,jsonify
 from dotenv import load_dotenv
 import os
@@ -13,7 +13,6 @@ app.secret_key = "supersecretkey"
 cred = credentials.Certificate("firebase_config.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-bucket = storage.bucket()
 load_dotenv()
 
 firebase_api_key = os.getenv("FIREBASE_API_KEY")
